6befddf19899a3d77608bb0e026bc132cd0aabac
AkiraUeda0523 <a.ueda0523@gmail.com>
2021-03-31 21:33:17 +0900

    最低限の実装完了

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -1,19 +1,30 @@
-//
-//  ViewController.swift
-//  FlappyBird
-//
-//  Created by 上田晃 on 2021/03/23.
-//
-
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // SKViewに型を変換する
+        let skView = self.view as! SKView
+        
+        // FPSを表示する
+        skView.showsFPS = true
+        
+        // ノードの数を表示する
+        skView.showsNodeCount = true
+        
+        // ビューと同じサイズでシーンを作成する
+        let scene = GameScene(size:skView.frame.size) // ←GameSceneクラスに変更する
+        
+        // ビューにシーンを表示する
+        skView.presentScene(scene)
     }
-
-
+    override var prefersStatusBarHidden: Bool {
+        get {
+            return true
+        }
+        
+    } // --- ここまで追加 ---
 }
-
